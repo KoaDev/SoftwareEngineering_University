67f5f422cdaf87e674d15e7e646d7ea80fe743f2
KoaDev <35237303+KoaDev@users.noreply.github.com>
2023-03-10 16:10:11 +0100

    Exercise2PrimeProduct

--- isp-lab-2-2023/src/main/java/isp/lab2/Exercise2PrimeProduct/Exercise2PrimeProduct.java
+++ isp-lab-2-2023/src/main/java/isp/lab2/Exercise2PrimeProduct/Exercise2PrimeProduct.java
@@ -6,13 +6,43 @@ public class Exercise2PrimeProduct {
 
     /**
      * This method should return the product of the first n prime numbers
-     * @param n
-     * @param m
-     * @return
+     * @param n the number of prime numbers
+     * @param m the number of prime numbers
+     * @return the product of the first n prime numbers
      */
     public static long getPrimeProduct(int n, int m) {
-        return 0;
+        long product = 1; // the product of the first n prime numbers
+        int count = 0; // the number of prime numbers found
+        int number = 2; // the first prime number
+        while (count < m) { // while the number of prime numbers found is less than m
+            if (isPrime(number)) { // if the number is prime
+                count++; // increment the number of prime numbers found
+                if (count >= n) { // if the number is in the range
+                    product *= number; // multiply the product with the number
+                }
+            }
+            number++; // increment the number
+        }
+        return product;
     }
+
+    /**
+     * This method should return true if the number is prime
+     * @param number the number to check
+     * @return true if the number is prime
+     */
+    public static boolean isPrime(int number) {
+        if (number <= 1) { // 1 is not prime
+            return false;
+        }
+        for (int i = 2; i < number; i++) { // check if the number is divisible by any number less than it
+            if (number % i == 0) { // if the number is divisible by i
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 
     /**
