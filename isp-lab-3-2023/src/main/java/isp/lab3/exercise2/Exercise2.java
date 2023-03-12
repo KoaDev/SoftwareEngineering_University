1f942bcc7970ba31b62ed50bd1bfc82d65d0f973
KoaDev <35237303+KoaDev@users.noreply.github.com>
2023-03-12 21:46:02 +0100

    Exercise2

--- isp-lab-3-2023/src/main/java/isp/lab3/exercise2/Exercise2.java
+++ isp-lab-3-2023/src/main/java/isp/lab3/exercise2/Exercise2.java
@@ -1,4 +1,45 @@
 package isp.lab3.exercise2;
 
 public class Exercise2 {
+    private int length;
+    private int width;
+    private String color;
+
+    public Exercise2() {
+        length = 2;
+        width = 1;
+        color = "red";
+    }
+
+    public Exercise2(int length, int width) {
+        this.length = length;
+        this.width = width;
+        color = "red";
+    }
+
+    public Exercise2(int length, int width, String color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    public int getArea() {
+        return length * width;
+    }
 }
