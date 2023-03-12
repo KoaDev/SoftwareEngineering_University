b9411813de99a653f960b8b375dd653773d1110a
KoaDev <35237303+KoaDev@users.noreply.github.com>
2023-03-12 14:37:32 +0100

    MathBasicOperationsTest

---
+++ isp-lab-1-2023/src/test/java/isp/lab1/MathBasicOperationsTest.java
@@ -0,0 +1,26 @@
+package isp.lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author mihai.hulea
+ */
+public class MathBasicOperationsTest {
+
+    @Test
+    public void testAdd(){
+        assertEquals("Should add 2 numbers", 30, MathBasicOperations.add(10,20));
+    }
+
+    @Test
+    public void testSubstract(){
+        assertEquals("Should substract 2 numbers", 5, MathBasicOperations.substract(15,10));
+    }
+
+    @Test
+    public void testMultiply(){
+        assertEquals("Should multiply 2 numbers", 100, MathBasicOperations.multiply(10,10));
+    }
+}
