0e319084096932b643340c756efcd4c64a6c1b36
KoaDev <35237303+KoaDev@users.noreply.github.com>
2023-03-24 15:28:54 +0100

    Exercise4

--- isp-lab-5-2023/src/main/java/isp/lab5/exercise4/Exercise4.java
+++ isp-lab-5-2023/src/main/java/isp/lab5/exercise4/Exercise4.java
@@ -2,3 +2,91 @@ package isp.lab5.exercise4;
 
 public class Exercise4 {
 }
+
+interface Shape {
+    /**
+     * Draw a specific shape
+     */
+    void draw();
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("A rectangle is drawn");
+    }
+}
+
+class RoundedRectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("A rounded rectangle is drawn");
+    }
+}
+
+abstract class AbstractShapeFactory {
+    /**
+     * Get specific shape by type
+     * @param type - one of the values ("rectangle", "roundedRectangle", "square", "roundedSquare")
+     * @return instance of shape by desired type
+     */
+    abstract Shape getShape(String type);
+}
+
+class ShapeFactory extends AbstractShapeFactory {
+    @Override
+    Shape getShape(String type) {
+        if ("rectangle".equals(type)) {
+            return new Rectangle();
+        }
+
+        return null;
+    }
+}
+
+class RoundedShapeFactory extends AbstractShapeFactory {
+    @Override
+    Shape getShape(String type) {
+        if ("roundedRectangle".equals(type)) {
+            return new RoundedRectangle();
+        }
+
+        return null;
+    }
+}
+
+class ShapeFactoryProvider {
+    private ShapeFactoryProvider() {
+    }
+
+    /**
+     * Get an instance of {@link AbstractShapeFactory} based on <param>type</param>
+     * @param type - one of the values "normal" / "rounded"
+     * @return instance of type {@link AbstractShapeFactory}
+     */
+    public static AbstractShapeFactory getShapeFactory(String type) {
+        if ("normal".equals(type)) {
+            return new ShapeFactory();
+        }
+
+        if ("rounded".equals(type)) {
+            return new RoundedShapeFactory();
+        }
+
+        return null;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        AbstractShapeFactory roundedShapeFactory = ShapeFactoryProvider.getShapeFactory("rounded");
+        AbstractShapeFactory normalShapeFactory = ShapeFactoryProvider.getShapeFactory("normal");
+
+        // create instances
+        Shape rectangle = roundedShapeFactory.getShape("roundedRectangle");
+        Shape shape = normalShapeFactory.getShape("rectangle");
+
+        rectangle.draw();
+        shape.draw();
+    }
+}
